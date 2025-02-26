d1890b8dd8d38193ba64f514af6f68b77133e48e
Parth Sarthi Mishra <90957276+mrsarthi@users.noreply.github.com>
2025-02-26 19:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ def form():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
